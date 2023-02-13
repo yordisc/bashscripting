a7cd4a615ee51e4b7e94fb48fd86e035f2658924
yordisc <yordiscujar@gmail.com>
2023-02-12 21:47:23 -0400

    Update mysqlworkbech-snap.sh

--- mysqlworkbech-snap.sh
+++ mysqlworkbech-snap.sh
@@ -31,9 +31,13 @@ function chiguire()
 			echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 
 }
-
+		echo " "
+		echo "Requiere tener instalado Snap"
+			sleep 2s
 		clear
 		chiguire
+		echo " "
+		echo "https://github.com/yordisc/"
 			sleep 2s
 		clear
 
@@ -41,16 +45,16 @@ function chiguire()
 
 			echo "#----------------------------Instalando Workbench MySQL Community - Snap (Comenzando en 5 segundos)-----------------------------#"
 			sleep 5s
-#Desistalador
+# Desistalador
 			sudo snap forget mysql --beta
 			sudo snap remove mysql --beta
 			sudo snap forget mysql-workbench-community
 			sudo snap remove mysql-workbench-community
 		clear
-#Workbench sql community - snap
+# Workbench sql community - snap
 			sudo snap install mysql --beta
 			sudo snap install mysql-workbench-community
 			sudo sysctl kernel.unprivileged_userns_clone=1
-#Agregar lanzadores snap
+# Agregar lanzadores snap
 			sudo cp /var/lib/snapd/desktop/applications/* /usr/share/applications/
 			echo "#----------------------------Instalado Workbench MySQL Community - Snap-----------------------------#"
